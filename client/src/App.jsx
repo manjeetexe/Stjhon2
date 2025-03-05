b5c4ed4db255af096660c51d5ea2ba87d37c6c17
manjeetexe <manjeetsharma9257@gmail.com>
2025-03-05 14:24:25 +0530

    new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Team from './page/Team';
 import Home from './page/Home';
-
-
+import Profile from './page/profile'
+import MSG from './page/MSG'
+import Search from './page/Search'
 const App = () => {
   return (
     <>
@@ -12,6 +13,9 @@ const App = () => {
           <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/" element={<Team />} />
+            <Route path="/userprofile" element={<Profile />} />
+            <Route path="/msg" element={<MSG />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
         </Router>
     </>
